967556d27460913f85cfbc6061a6ec033556c0b3
Trent Shailer <trent.shailer@gmail.com>
2024-04-13 21:38:55 +1200

    basic enemy animation setup

--- scripts/enemy/basic_enemy.gd
+++ scripts/enemy/basic_enemy.gd
@@ -2,8 +2,11 @@ class_name BasicEnemy
 extends Enemy
 
 enum AttackState { NONE, CHARGING, LUNGING, RECHARGING }
+enum AnimationState { IDLE, RUN, CHARGE, LUNGE, RECHARGE}
+enum AnimationDirection {UP, DOWN, LEFT, RIGHT}
 
 @onready var player = Singleton.player_node
+@onready var animator: AnimatedSprite2D = $AnimatedSprite2D
 
 @export_category("Attack Settings")
 @export var range = 50
@@ -12,11 +15,70 @@ enum AttackState { NONE, CHARGING, LUNGING, RECHARGING }
 @export var lunge_distance: float = 100
 @export var recharge_time: float = 1
 
-
 var attack_state = AttackState.NONE
 var current_charge_time = 0
 var attack_direction: Vector2
 var charge_position: Vector2
+var animation_state = AnimationState.IDLE
+var animation_direction = AnimationDirection.RIGHT
+
+func animation_state_to_string(animation_state: AnimationState) -> String:
+	match animation_state:
+		AnimationState.IDLE:
+			return "idle"
+		AnimationState.RUN:
+			return "run"
+		AnimationState.CHARGE:
+			return "charge"
+		AnimationState.LUNGE:
+			return "lunge"
+		AnimationState.RECHARGE:
+			return "recharge"
+		_:
+			return "idle"
+
+func animation_direction_to_string(animation_durection: AnimationDirection) -> String:
+	match animation_direction:
+		AnimationDirection.UP:
+			return "up"
+		AnimationDirection.RIGHT:
+			return "right"
+		AnimationDirection.DOWN:
+			return "down"
+		AnimationDirection.LEFT:
+			return "left"
+		_:
+			return "right"
+
+func get_animation_name() -> String:
+	return animation_state_to_string(animation_state) + "_" + animation_direction_to_string(animation_direction)
+
+func _process(delta: float) -> void:
+	if velocity.dot(Vector2.RIGHT) > 0.5:
+		animation_direction = AnimationDirection.RIGHT
+	elif velocity.dot(Vector2.UP) > 0.5:
+		animation_direction = AnimationDirection.UP
+	elif velocity.dot(Vector2.LEFT) > 0.5:
+		animation_direction = AnimationDirection.LEFT
+	elif velocity.dot(Vector2.DOWN) > 0.5:
+		animation_direction = AnimationDirection.DOWN
+	
+	match attack_state:
+		AttackState.NONE:
+			if velocity.length() > 1:
+				animation_state = AnimationState.RUN
+			else:
+				animation_state = AnimationState.IDLE
+		AttackState.CHARGING:
+			animation_state = AnimationState.CHARGE
+		AttackState.LUNGING:
+			animation_state = AnimationState.LUNGE
+		AttackState.RECHARGING:
+			animation_state = AnimationState.RECHARGE
+	
+	if animator.animation != get_animation_name():
+		animator.stop()
+		animator.play(get_animation_name())
 
 func _set_movement_target():
 	navigation_agent.target_position = player.global_position
