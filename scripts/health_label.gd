bd443c8a68856db893b94903ea2887ee4eb5241b
Heihei03 <jcheihei2003@gmail.com>
2024-04-13 16:48:52 +1200

    Update health text colour

--- scripts/health_label.gd
+++ scripts/health_label.gd
@@ -1,5 +1,6 @@
 extends Label
 
+@onready var health = Singleton.player_node.player_health
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +9,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	text = "HP: " + str(Singleton.player_node.player_health)
+	text = "HP: " + str(health)
+	if health > 75:
+		set("theme_override_colors/font_color", Color(0.0,1.0,0.0,1.0))
+	if health < 25;
+		set("theme_override_colors/font_color", Color(1.0,0.0,0.0,1.0))
