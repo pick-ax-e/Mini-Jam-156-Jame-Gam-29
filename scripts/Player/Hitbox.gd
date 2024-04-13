afcde761433e4e38bbfc97f8f1f2e45531bc1edd
oneye5 <ocjlaz@gmail.com>
2024-04-13 19:26:06 +1200

    comment out broken code

--- scripts/Player/Hitbox.gd
+++ scripts/Player/Hitbox.gd
@@ -6,10 +6,11 @@ func _ready():
 	hitbox = $Hitbox1
 func updateRot(vec2Dir:Vector2):
 	hitbox.target_position = vec2Dir.normalized() * length
-func _physics_process(delta):
-	hits = hitbox.collision_result
-	for hit in hits:
-		if hit is Enemy:
-			var enemy: Enemy = hit
-				
+
+#func _physics_process(delta):
+	#hits = hitbox.collision_result
+	#for hit in hits:
+		#if hit is Enemy:
+			#var enemy: Enemy = hit
+			
 	
