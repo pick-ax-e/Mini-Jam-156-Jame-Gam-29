cfacb9ed351925ebbf1f11b1379e906259143352
Trent Shailer <trent.shailer@gmail.com>
2024-04-13 14:44:17 +1200

    update basic enemy

--- scripts/enemy/basic_enemy.gd
+++ scripts/enemy/basic_enemy.gd
@@ -3,6 +3,7 @@ extends Enemy
 
 enum AttackState { NONE, CHARGING, LUNGING, RECHARGING }
 
+@export_category("Attack Settings")
 @export var range = 50
 @export var charge_time: float = 1
 @export var lunge_speed: float = 100
@@ -25,18 +26,27 @@ func _movement(delta: float):
 	if position.distance_to(player.position) <= range:
 	var player = Singleton.player_node
 	print(player)
+	
+	if global_position.distance_to(player.global_position) <= range:
 		return
 		
 	move_and_collide(position.direction_to(player.position) * speed * delta)
 
 # Basic enemy can attack if it is within x units of the player
 func _can_attack() -> bool:
+	var player = Singleton.player_node
+	
+	return global_position.distance_to(player.global_position) <= range && !is_attacking
 
 # charges up lunge, then captures the direction to the player and lunges towards them
 func _do_attack(delta: float):
+	var player = Singleton.player_node
+	
 	match attack_state:
 		AttackState.NONE:
+			attack_direction = global_position.direction_to(player.global_position)
 			attack_state = AttackState.CHARGING
+			charge_position = global_position
 		AttackState.CHARGING:
 			charge_attack(delta)
 		AttackState.LUNGING:
@@ -53,6 +63,7 @@ func charge_attack(delta: float):
 	current_charge_time += delta
 
 func lunge(delta: float):
+	if charge_position.distance_to(global_position) >= lunge_distance:
 		attack_state = AttackState.RECHARGING
 		return
 		
