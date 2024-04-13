ee1adb28746dd0169adf70871bbda7c0e96e5016
Heihei03 <jcheihei2003@gmail.com>
2024-04-13 12:59:23 +1200

    Fix timer signal connection

--- scripts/singleton.gd
+++ scripts/singleton.gd
@@ -8,14 +8,16 @@ func _ready():
 	timer = Timer.new()
 	add_child(timer)
 	timer.start(300)
+	timer.timeout.connect(_on_timer_timeout)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if timer.timeout():
-		reset()
-		emit_signal("Reset")
+	pass
 
+func _on_timer_timeout():
+	reset()
+	emit_signal("Reset")
 
 func reset():
 	pass
