ea2dd2e1794de745a2f8fe0244aebcc51720f394
Trent Shailer <trent.shailer@gmail.com>
2024-04-13 16:57:25 +1200

    WIP enemy navmesh stuff and hit method on enemy

--- scripts/enemy/enemy.gd
+++ scripts/enemy/enemy.gd
@@ -5,8 +5,8 @@ enum State { IDLE, ACTIVE }
 
 @export_category("Enemy Settings")
 @export var type: String
-@export var health: int
-@export var attack_damage: int
+@export var health: float
+@export var attack_damage: float
 @export var speed: float
 
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
@@ -15,6 +15,9 @@ enum State { IDLE, ACTIVE }
 var state = State.IDLE
 var is_attacking = false
 var target_position: Vector2
+
+func _ready():
+	navigation_agent.velocity_computed.connect(Callable(_on_velocity_computed))
 
 func _physics_process(delta: float):
 	if state == State.IDLE:
@@ -39,14 +42,25 @@ func _physics_process(delta: float):
 
 # Handles the movement of an enemy
 func move_to_target(delta: float):
-	var target = navigation_agent.get_next_path_position()
-	
 	if navigation_agent.is_navigation_finished() || is_attacking:
+		print("Test")
+		navigation_agent.set_velocity_forced(Vector2.ZERO)
+		velocity = Vector2.ZERO
+		#move_and_slide()
 		return
 		
-	var direction = global_position.direction_to(target)
+	var target = navigation_agent.get_next_path_position()
+	var new_velocity = global_position.direction_to(target) * speed
 	
-	move_and_collide(direction * speed * delta)
+	if navigation_agent.avoidance_enabled:
+		navigation_agent.velocity = new_velocity
+	else:
+		_on_velocity_computed(new_velocity)
+
+func _on_velocity_computed(safe_velocity: Vector2):
+	print("test")
+	velocity = safe_velocity
+	move_and_slide()
 
 func _set_movement_target():
 	pass
@@ -60,4 +74,7 @@ func _can_attack() -> bool:
 
 # perform the attack
 func _do_attack(delta: float) -> void:
+	pass
+
+func take_damage(damage: float):
 	pass
