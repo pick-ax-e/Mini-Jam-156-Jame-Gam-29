bce58d468e7c2a9e0b8a4b4b21d88047ede57f43
Heihei03 <jcheihei2003@gmail.com>
2024-04-13 16:50:58 +1200

    Fix variable

--- scripts/health_label.gd
+++ scripts/health_label.gd
@@ -1,14 +1,15 @@
 extends Label
 
-@onready var health = Singleton.player_node.player_health
+@onready var health
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	health = 100
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	health = Singleton.player_node.player_health
 	text = "HP: " + str(health)
 	if health > 75:
 		set("theme_override_colors/font_color", Color(0.0,1.0,0.0,1.0))
