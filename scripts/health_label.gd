a9d4f48b142a1343fa75638936b1bed9f10aa6b2
Heihei03 <jcheihei2003@gmail.com>
2024-04-13 16:49:19 +1200

    Fix typo

--- scripts/health_label.gd
+++ scripts/health_label.gd
@@ -12,5 +12,5 @@ func _process(delta):
 	text = "HP: " + str(health)
 	if health > 75:
 		set("theme_override_colors/font_color", Color(0.0,1.0,0.0,1.0))
-	if health < 25;
+	if health < 25:
 		set("theme_override_colors/font_color", Color(1.0,0.0,0.0,1.0))
