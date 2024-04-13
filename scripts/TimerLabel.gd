352b736ab1f65b840b780dc841c999e133882073
Heihei03 <jcheihei2003@gmail.com>
2024-04-13 16:44:40 +1200

    Timer turns red when 10 secs or under

--- scripts/TimerLabel.gd
+++ scripts/TimerLabel.gd
@@ -18,7 +18,11 @@ func update():
 	var minutes_str = str(minutes)
 	var seconds_str = str(seconds)
 	
+	set("theme_override_colors/font_color", Color(0.0,0.0,0.0,1.0))
+	
 	if seconds < 10:
 		seconds_str = "0" + seconds_str
+		if minutes == 0:
+			set("theme_override_colors/font_color", Color(1.0,0.0,0.0,1.0))
 	
 	text = minutes_str + ":" + seconds_str
