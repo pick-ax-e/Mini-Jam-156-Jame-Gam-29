cab3f86e8666e2ee4a5015b3a1c63f4f560876a9
Heihei03 <jcheihei2003@gmail.com>
2024-04-13 12:38:55 +1200

    Add timer

--- scripts/singleton.gd
+++ scripts/singleton.gd
@@ -1,11 +1,20 @@
 extends Node
 
+var timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	timer = Timer.new()
+	add_child(timer)
+	timer.start(300)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if timer.timeout():
+		reset()
+		emit_signal("Reset")
+
+
+func reset():
 	pass
