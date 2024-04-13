6316f51189be123f3d6c11f01794749d804ef919
oneye5 <ocjlaz@gmail.com>
2024-04-13 19:17:36 +1200

    hitbox for player

---
+++ scripts/Player/Hitbox.gd
@@ -0,0 +1,15 @@
+extends ShapeCast2D
+var hitbox: ShapeCast2D
+const length = 100;
+var hits
+func _ready():
+	hitbox = $Hitbox1
+func updateRot(vec2Dir:Vector2):
+	hitbox.target_position = vec2Dir.normalized() * length
+func _physics_process(delta):
+	hits = hitbox.collision_result
+	for hit in hits:
+		if hit is Enemy:
+			var enemy: Enemy = hit
+				
+	
