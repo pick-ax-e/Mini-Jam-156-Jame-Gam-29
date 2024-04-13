1087d0555988cc69dc6ff866e456fd8b00478eed
Heihei03 <jcheihei2003@gmail.com>
2024-04-13 17:08:15 +1200

    Update health font colour

--- scripts/health_label.gd
+++ scripts/health_label.gd
@@ -13,5 +13,9 @@ func _process(delta):
 	text = "HP: " + str(health)
 	if health > 75:
 		set("theme_override_colors/font_color", Color(0.0,1.0,0.0,1.0))
+	if 50 < health < 75:
+		set("theme_override_colors/font_color", Color(1.0,1.0,0.0,1.0))
+	if 25 < health < 50:
+		set("theme_override_colors/font_color", Color(1.0,0.75,0.0,1.0))
 	if health < 25:
 		set("theme_override_colors/font_color", Color(1.0,0.0,0.0,1.0))
